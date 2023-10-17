6740aa765460988d0663c7fa1e08a1c5806acb1c
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-10-17 11:28:00 +0900

    카테고리

--- src/main/java/com/example/beanterestful/controllers/CategoryController.java
+++ src/main/java/com/example/beanterestful/controllers/CategoryController.java
@@ -1,4 +1,52 @@
 package com.example.beanterestful.controllers;
 
+import com.example.beanterestful.dto.CategoryDto;
+import com.example.beanterestful.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/categories")
 public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryDto> categoryId(@PathVariable Long category_id) {
+        return ResponseEntity.ok(categoryService.getCategoryById(category_id));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
+        CategoryDto createdCategoryDto = categoryService.createCategory(categoryDto);
+
+        if (createdCategoryDto != null) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdCategoryDto);
+        } else {
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CategoryDto> updateCategory(@PathVariable Long category_id, @RequestBody CategoryDto categoryDto) {
+        try {
+            CategoryDto updatedCategoryDto = categoryService.updateCategory(category_id, categoryDto);
+
+            if (updatedCategoryDto != null) {
+                return ResponseEntity.ok(updatedCategoryDto);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
 }
--- src/main/java/com/example/beanterestful/repository/CategoryRepository.java
+++ src/main/java/com/example/beanterestful/repository/CategoryRepository.java
@@ -1,4 +1,7 @@
 package com.example.beanterestful.repository;
 
-public class CategoryRepository {
+import com.example.beanterestful.entity.CategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
 }
